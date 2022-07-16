8f37ee6626243fe4c8f51a6a1871c2cc18e236ee
leonelSubelza <subelza150@gmail.com>
2022-07-16 15:02:11 -0300

    Se agrega proyecto base para el Backend

---
+++ Backend/curso-java-avanzado/src/main/java/com/cursojavaavanzado/cursojavaavanzado/CursoJavaAvanzadoApplication.java
@@ -0,0 +1,13 @@
+package com.cursojavaavanzado.cursojavaavanzado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJavaAvanzadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJavaAvanzadoApplication.class, args);
+	}
+
+}
---
+++ Backend/curso-java-avanzado/src/test/java/com/cursojavaavanzado/cursojavaavanzado/CursoJavaAvanzadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursojavaavanzado.cursojavaavanzado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJavaAvanzadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
